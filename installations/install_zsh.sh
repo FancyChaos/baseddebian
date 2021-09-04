ccfa2b7be90c98df6bf3217ece8dc6a54f87fc88
FancyChaos <Felixs.Developer@tutanota.com>
2021-09-04 10:18:36 -0500

    Full zsh and oh-my-zsh install

--- installations/install_zsh.sh
+++ installations/install_zsh.sh
@@ -3,11 +3,32 @@
 # Install zsh
 sudo apt install zsh -y
 
-### Cant manage to install oh my zsh automatically, dont care for now
-# Install oh my zsh
-#export CHSH=yes
-#export RUNZSH=no
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Set zsh as default shell (Also set SHELL in .xinitrc)
 sudo chsh -s $(which zsh)
+
+# Install oh-my-zsh
+cd /tmp/
+export RUNZSH=no
+export CHSH=no
+export KEEP_ZSHRC=yes
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install autocomplete
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install zsh-completion
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+# Install k
+git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
+
+
+# Install autojump
+cd /tmp/
+git clone git://github.com/wting/autojump.git
+cd autojump/
+python3 install.py
+
