fab173b44cb6d004344320bdb8d4e5981130959d
FancyChaos <Felixs.Developer@tutanota.com>
2021-09-04 06:24:19 -0500

    Install custom version of st-instantos

--- installations/install_st.sh
+++ installations/install_st.sh
@@ -7,9 +7,10 @@
 # git fetch origin
 # ./build.sh
 
-# Build st by instantOS
+# Build st by instantOS (Custom version with "scrollback" patch)
 cd /tmp/
-git clone https://github.com/instantOS/st-instantos.git
+# git clone https://github.com/instantOS/st-instantos.git
+git clone https://github.com/FancyChaos/st-instantos.git
 cd st-instantos
 make
 sudo make install
