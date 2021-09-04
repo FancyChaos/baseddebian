c46ad939395cabb466896af02fd6548eab6b9159
FancyChaos <Felixs.Developer@tutanota.com>
2021-09-04 12:06:26 +0200

    Install st version of instantOS

--- installations/install_st.sh
+++ installations/install_st.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# # Build custom st
+# cd /tmp/
+# git clone https://github.com/FancyChaos/st.git
+# cd st/
+# git fetch origin
+# ./build.sh
+
+# Build st by instantOS
 cd /tmp/
-git clone https://github.com/FancyChaos/st.git
-cd st/
-git fetch origin
-./build.sh
+git clone https://github.com/instantOS/st-instantos.git
+cd st-instantos
+make
+sudo make install
