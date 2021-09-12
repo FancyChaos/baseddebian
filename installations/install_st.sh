80e68c16be1d6774c67ed6bd0fa202629be01b37
FancyChaos <Felixs.Developer@tutanota.com>
2021-09-12 13:19:24 +0200

    Remove comments

--- installations/install_st.sh
+++ installations/install_st.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# # Build custom st
-# cd /tmp/
-# git clone https://github.com/FancyChaos/st.git
-# cd st/
-# git fetch origin
-# ./build.sh
-
 # Build st by instantOS (Custom version with "scrollback" patch)
 cd /tmp/
-# git clone https://github.com/instantOS/st-instantos.git
 git clone https://github.com/FancyChaos/st-instantos.git
 cd st-instantos/
 rm config.h || true
