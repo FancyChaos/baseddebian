455f867d6ef43cb6fe20330f197d2f1136e3685b
FancyChaos <Felixs.Developer@tutanota.com>
2021-09-16 21:46:44 +0200

    Add instantMENU and instantLOCK

---
+++ installations/16-install_instantmenu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /tmp/
+git clone https://github.com/instantOS/instantMENU
+cd instantMENU/
+chmod +x build.sh
+./build.sh
---
+++ installations/17-install_instantlock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /tmp/
+git clone https://github.com/FancyChaos/instantlock.git
+cd instantlock/
+chmod +x build.sh
+./build.sh
