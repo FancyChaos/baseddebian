fac88fd5013cc698c55bed2206dc8d4a5c0afb48
FancyChaos <Felixs.Developer@tutanota.com>
2021-11-04 20:08:38 +0100

    Add build for devuan

--- install.sh
+++ install.sh
@@ -60,33 +60,15 @@ ssh-keygen -q -f $HOME/.ssh/git_key -t ecdsa -b 521 -N ""
 ### Fix broken packages for good measure (why not?)
 sudo apt install -f -y
 
-### Deactivate systemd-networkd for good measure
-sudo systemctl disable systemd-networkd.service
-sudo systemctl enable NetworkManager.service
-
 ### Cleanup
 sudo apt autoremove -y
 sudo apt remove python-is-python2 -y
 sudo ln -s $(which python3) /usr/local/bin/python
 
-sudo systemctl disable unattended-upgrades.service
-sudo systemctl disable cups.service
-sudo systemctl disable exim4.service
-sudo systemctl disable bluetooth.service
-sudo systemctl disable blueman-mechanism.service
-
-# Disable tracker (Data indexing for GNOME mostly)
-systemctl --user mask tracker-store.service tracker-miner-fs.service tracker-miner-rss.service tracker-extract.service tracker-miner-apps.service tracker-writeback.service
-systemctl --user mask gvfs-udisks2-volume-monitor.service gvfs-metadata.service gvfs-daemon.service
-
 ### Disable webcam by default
 ### Toogle back on with 'sudo modprobe uvcvideo'
-# sudo modprobe -r uvcvideo
-
-### Boot into command line
-sudo systemctl set-default multi-user.target
+sudo modprobe -r uvcvideo
 
 ### Done
 echo "Installation done"
-sudo reboot
-
+#sudo reboot
