9bd572c204174a6e9b1068e6a4c7d337aafc1b5a
FancyChaos <Felixs.Developer@tutanota.com>
2022-02-08 19:09:29 +0100

    Fix spotify installation

--- installations/40-install_spotify.sh
+++ installations/40-install_spotify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update -y && sudo apt-get install -y spotify-client
