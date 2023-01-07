2993d2f780f5c7a5b153130fc4dd356139aafdfe
FancyChaos <Felixs.Developer@tutanota.com>
2023-01-07 22:08:52 +0100

    Build picom 10.2 from github instead of using the package

---
+++ installations/55-install_picom.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson
+
+cd /tmp/
+git clone https://github.com/yshui/picom.git
+cd picom/
+
+git submodule update --init --recursive
+git checkout v10.2
+meson setup --buildtype=release . build
+sudo ninja -C build
