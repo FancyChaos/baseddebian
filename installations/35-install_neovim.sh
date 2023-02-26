6092cd1de077854c09534d11fb5193cae5278acf
FancyChaos <Felixs.Developer@tutanota.com>
2023-02-26 14:27:45 +0100

    Use lazyvim for neovim

--- installations/35-install_neovim.sh
+++ installations/35-install_neovim.sh
@@ -4,16 +4,21 @@ cd /tmp/
 
 git clone https://github.com/neovim/neovim.git
 cd neovim/
-git checkout tags/v0.8.2
+git checkout tags/v0.8.3
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 
 cd /tmp/
 
 ### Install neovim plugins
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 pip3 install --user neovim || pip install --user neovim
 
-mkdir -p $HOME/.config/nvim/plugged/ || true
 
-nvim -c PlugInstall -c UpdateRemotePlugins -c quitall
+# Install lazyvim
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.cache/nvim ~/.cache/nvim.bak
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
--- installations/40-install_spotify.sh
+++ installations/40-install_spotify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/spotify.gpg &>/dev/null
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb [signed-by=/usr/share/keyrings/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update -y && sudo apt-get install -y spotify-client
