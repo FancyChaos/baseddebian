faddec563284b6333dcc1942430e5b7a9034a51a
FancyChaos <Felixs.Developer@tutanota.com>
2023-03-21 17:00:19 +0100

    Fix Spicetify installation

--- installations/41-install_spicetify.sh
+++ installations/41-install_spicetify.sh
@@ -4,12 +4,15 @@ cd /tmp/
 
 # Setup our PATH and adjust permissions for spotify
 export PATH=$PATH:/home/${USER}/.spicetify
-chown -R ${USER}:root /usr/share/spotify/
+sudo chown -R ${USER}:root /usr/share/spotify/
 
 # Download and install spicetify
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh
+# Backup spotify (Required first command)
+spicetify backup apply
+
+# Download and install spicetify marketplace
 curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh
 
-# Install chosen theme
+# Install Nord-Spotify theme
 curl -fsSL https://raw.githubusercontent.com/Tetrax-10/Nord-Spotify/master/install-scripts/install.sh | sh
-#curl -fsSL https://raw.githubusercontent.com/NYRI4/Comfy-spicetify/main/install.sh | sh
