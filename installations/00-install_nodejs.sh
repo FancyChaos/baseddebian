0510bd18996f0c1476c6b29d0b313caa2cb66d09
FancyChaos <Felixs.Developer@tutanota.com>
2024-03-28 21:37:27 +0100

    Modernize nodejs installation

--- installations/00-install_nodejs.sh
+++ installations/00-install_nodejs.sh
@@ -2,12 +2,11 @@
 
 # Install nodejs 20
 
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+# installs NVM (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-sudo apt-get update
-sudo apt-get install nodejs -y
+# download and install Node.js
+nvm install 20
